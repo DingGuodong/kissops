cdd2898f4139c3f6592885601c5eed37410c3bd6
DingGuodong <uberurey_ups@163.com>
2017-03-16 23:22:46 +0800

    add gevent support and open default web browser with default app url

--- manage-cli/runserver.py
+++ manage-cli/runserver.py
@@ -8,15 +8,33 @@ User:               Guodong
 Create Date:        2016/9/26
 Create Time:        10:34
  """
+from gevent import monkey
+
+monkey.patch_all()
 import os
 import sys
+import gevent
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-os.chdir(BASE_DIR)
-print "Admin --> http://127.0.0.1:8000/admin/"
-print "App   --> http://127.0.0.1:8000/admin/itoms/app/"
-try:
-    os.system("python manage.py runserver")
-except OSError as e:
-    print e.message
-    sys.exit(1)
+admin_url = 'http://127.0.0.1:8000/admin/'
+app_url = 'http://127.0.0.1:8000/admin/itoms/app/'
+
+
+def runserver():
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    os.chdir(BASE_DIR)
+    try:
+        gevent.sleep(0.2)
+        print "Admin --> %s" % admin_url
+        print "App   --> %s" % app_url
+        os.system("python manage.py runserver")
+    except OSError as e:
+        print e.message
+        sys.exit(1)
+
+
+def openwebbrowser():
+    import webbrowser
+    webbrowser.open(app_url)
+
+
+gevent.joinall([gevent.spawn(runserver), gevent.spawn(openwebbrowser)])
