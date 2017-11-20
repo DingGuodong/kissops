6f956bce676b6c7efd4f1d6311577c1512b08d9d
DingGuodong <uberurey_ups@163.com>
2017-11-20 19:27:33 +0800

    update djangorestframework package and setting

--- kissops/environments/local_settings.py
+++ kissops/environments/local_settings.py
@@ -109,7 +109,8 @@ REST_FRAMEWORK = {
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
         'rest_framework.permissions.IsAdminUser',
     ],
-    'PAGE_SIZE': 10
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 MIDDLEWARE_CLASSES = [
